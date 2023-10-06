0f42cde6dc826edef5fd84abe3e633c43049c8cf
spogoretskyi <spogoretskyi@gmail.com>
2023-10-07 00:53:12 +0300

    CLI bot was added

---
+++ cli_bot.py
@@ -0,0 +1,69 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    try:
+        name, phone = args
+    except  ValueError:
+        return "Values should be name and phone."
+    #should be checked for phone number, out of scope
+    contacts[name.lower()] = phone
+    return "Contact added."
+
+def add_username_phone(args, contacts):
+    try:
+        name = args
+    except  ValueError:
+        return "Value should be name."
+    name = ''.join(name).lower()
+    if is_contact_exists(name, contacts):
+        return contacts[name]
+    else:
+        return "No such contact in the list."
+
+def change_username_phone(args, contacts):
+    try:
+        name, phone = args
+    except  ValueError:
+        return "Values should be name and phone."
+    name = ''.join(name).lower()
+    #should be checked for phone number, out of scope
+    phone = ''.join(phone)
+    if is_contact_exists(name, contacts):
+        contacts[name] = phone
+        return "Phone was changed."
+    else:
+        return "No such contact in the list."
+    
+def is_contact_exists(name, contacts):
+     if contacts.get(name) != None:
+         return True
+     return False
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command.strip() in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_username_phone(args, contacts))
+        elif command == "phone":
+            print(add_username_phone(args, contacts))
+        elif command == "all":
+            print(contacts)
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
